bb0d5bcf38c4ca8dbccc6760d071a23785232608
feilong <fanfeilong@outlook.com>
2024-07-29 16:31:02 +0800

    add usage for agent.py

--- agent.py
+++ agent.py
@@ -7,6 +7,10 @@ from pyeff.fs import current_dir
 if __name__ == "__main__":
     """
     pip install -r requirements-dev.txt
+    python agent.comment --token=...
+    python agent.test
+
+    TODO: chain agent.comment and agent.test
     """
 
     dir = current_dir(__file__)
@@ -19,10 +23,12 @@ if __name__ == "__main__":
 
     app.use(
         router=Router(
-            agent=Router(comment=CommentAgent()),
-            test=lambda config, options: run_cmds(
-                [f"pip install {dir}", "cd src/tests && python test.py"]
-            ),
+            agent=Router(
+                comment=CommentAgent(),
+                test=lambda config, options: run_cmds(
+                    [f"pip install {dir}", "cd src/tests && python test.py"]
+                ),
+            )
         )
     )
 
